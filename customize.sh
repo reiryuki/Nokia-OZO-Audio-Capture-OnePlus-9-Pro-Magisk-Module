011a2125240b56c930af8cd8a0156d508cc88fd2
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2025-06-22 21:11:06 +0700

    v1.3

--- customize.sh
+++ customize.sh
@@ -123,7 +123,6 @@ if [ "$KSU" == true ]; then
   ui_print " KSUVersion=$KSU_VER"
   ui_print " KSUVersionCode=$KSU_VER_CODE"
   ui_print " KSUKernelVersionCode=$KSU_KERNEL_VER_CODE"
-  sed -i 's|#k||g' $MODPATH/post-fs-data.sh
 else
   ui_print " MagiskVersion=$MAGISK_VER"
   ui_print " MagiskVersionCode=$MAGISK_VER_CODE"
@@ -348,14 +347,6 @@ if [ -f $MODPATH/system$DIR/$LIB ]\
 fi
 LIBS="libhidltransport.so libhwbinder.so"
 find_file
-
-# sepolicy
-FILE=$MODPATH/sepolicy.rule
-DES=$MODPATH/sepolicy.pfsd
-if [ "`grep_prop sepolicy.sh $OPTIONALS`" == 1 ]\
-&& [ -f $FILE ]; then
-  mv -f $FILE $DES
-fi
 
 # .aml.sh
 mv -f $MODPATH/aml.sh $MODPATH/.aml.sh
